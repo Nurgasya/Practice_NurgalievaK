658280c06ae96d2bdbb5007cae4b2434582968b7
Nurgasya <nurgalieva.xen@yandex.ru>
2025-01-19 23:21:01 +0900

    Add files via upload

---
+++ practice1.kt
@@ -0,0 +1,90 @@
+
+/*//Задание 1
+fun main(){
+    println("Типы данных.")
+    println("1.Целочисленные типы.")
+    println("Int: хранит целое число -2147483648 (-231) до 2147483647 (231-1) и занимает 4 байта");
+    println("2.Строки")
+    println("Строки представлены типом String. Строка представляет последовательность символов, заключенную в двойные кавычки, либо тройные двойные кавычки.")
+    println("3.Числа с плавающей точкой")
+    println("4.Логический тип Boolean")
+    println("5.Символы")
+}*/
+
+/*//Задание 2
+fun main(){
+var number:Double=1.12
+    println(number)
+}*/
+
+//Задание 3
+/*
+fun main() {
+   val E:Double = 2.718281828459045
+    val str =String.format("%.1f",E)
+    println(str)
+
+}*/
+
+
+//Задание 4
+/*fun main(){
+    println("Введите число")
+    var a=readln()
+    print("Вы ввели число:");print(a)
+}*/
+
+//Задание 5
+/*fun main() {
+    println("Введите число")
+    var a = readln()
+    print(a);print(" - вот такое число Вы ввели")
+}*/
+
+//Задание 6
+/*
+fun main(){
+    print(1);print(" "); print(13);print(" ");print(49)
+}*/
+
+// Задание 7
+/*
+fun main(){
+    print("7");print(" ");print(" ");print("15");print(" ");print(" ");print("100")
+}*/
+//Задание 8
+/*fun main( ){
+    println("Введите 3 числа:")
+    var a= readln()
+    var b= readln()
+    var c= readln()
+    print(a);print(" ");print(" ");print(b);print(" ");print(" ");print(c)
+}*/
+//Задание 9
+/*
+fun main( ) {
+    println("Введите 4 числа:")
+    var a = readln()
+    var b = readln()
+    var c = readln()
+    var n = readln()
+    print(a);print(" ");print(b);print(" ");print(c);print(" ");print(n)
+}*/
+//Задание 10
+/*fun main(){
+    println(50);println(10)
+}*/
+//Задание 11
+/*
+fun main(){
+    println(5);println(10);println(21)
+}*/
+//Задание 12
+/*fun main(){
+    println("Введите 4 любых числа")
+    var a=readln()
+    var b=readln()
+    var c=readln()
+    var n=readln()
+    println("Вывод ваших чисел 'столбиком'");println(a);println(b);println(c);println(n)
+}*/
